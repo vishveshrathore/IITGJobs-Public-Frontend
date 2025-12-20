3ccb5eb13fb5ff31ee18f7aefcd34d090d9e8964
Vishvesh Rathore <vishvesh.rathore.dev@gmail.com>
2025-12-20 15:42:56 +0530

    Your message

---
+++ dist/assets/Recuitment-NX_gTitE.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-C63xejpN.js";import r from"./ContactUs-VFRQKjuw.js";import"./config-BngW1GPq.js";const m=()=>t.jsx("div",{children:t.jsx(r,{})});export{m as default};
---
+++ dist/assets/config-BngW1GPq.js
@@ -0,0 +1,1 @@
+const t="https://iitgjobs-d5vi9.ondigitalocean.app";export{t as B};
